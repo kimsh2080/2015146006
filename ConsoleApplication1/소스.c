901916bde194a10fc95807bc04e6882d31cee322
unknown <kimsh2080@naver.com>
2020-10-28 21:26:17 +0900

    test

---
+++ ConsoleApplication1/소스.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+int main(void) {
+	int n, i, last;
+	int count = 0;
+	double x[20];
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf("%lf", &x[i]);
+		last = x[i];
+	}
+
+	for ()
+
+	// ¹è¿­ ½ºÄµ
+
+	for (i = 0; i < n; i++) {
+		if (last > x[i]) {
+			count++;
+		}
+	}
+	printf("%d", count);
+	return 0;
+}
